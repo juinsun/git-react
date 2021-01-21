187620128d9900fc5aa12ba5208a5269ed018222
juinsun <qwe506@naver.com>
2021-01-21 22:58:39 +0900

    이벤트 버튼 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './css/common.css';
 import './css/front.css';
 
 function App() {
+
+  const eventHandler = ()=>{
+    alert("안녕하세요");
+  }
   return (
     <div className="App">
       <div>
@@ -10,13 +14,13 @@ function App() {
         <div className="section-header">
           <span>헬로우 월드</span><br/>
           <talbe className="table">
-            s 
             <tr>
               <td>상자</td>
             </tr>
           </talbe>
         </div>
       </div>
+      <button onClick={eventHandler}>alert 버튼</button>
     </div>
   );
 }
