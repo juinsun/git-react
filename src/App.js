341d819091375312cffc8aec08b125e24d8cc847
juinsun <qwe506@naver.com>
2021-01-21 22:27:02 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import './css/common.css';
+import './css/front.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <div>개발 블로그</div>
+        <div className="section-header">
+          <span>헬로우 월드</span><br/>
+          <talbe className="table">
+            <tr>
+              <td>상자</td>
+            </tr>
+          </talbe>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default App;
